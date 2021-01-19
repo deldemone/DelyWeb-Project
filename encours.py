dd03dbdf32c281d04bc4cca101b5889792785848
DelDemone <delphine.demongeot@gmail.com>
2021-01-19 19:56:17 +0100

    Description

--- encours.py
+++ encours.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""Description: Permet de scanner le reseau d'un domaine et de parametrer les clients à distances"""
+
+__author__ = "Delphine Demongeot"
+__version__ = "1.0.0"
+__email__ = "delphine.demongeot@gmail.com"
+__status__ = "En cours de rédaction"
