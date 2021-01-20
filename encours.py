85659539d327586ca2fc584315fa759b1cd77663
DelDemone <delphine.demongeot@gmail.com>
2021-01-20 14:48:38 +0100

    Dépot de la clé publique sur le client

--- encours.py
+++ encours.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 ###Description: Permet de scanner le reseau d'un domaine et de parametrer les clients à distance"""
 
 __author__ = "Delphine Demongeot"
@@ -6,16 +8,15 @@ __email__ = "delphine.demongeot@gmail.com"
 __status__ = "En cours de rédaction"
 
 # Variables globales
-fichiermdp = open('/root/.pwd')
-pwd = fichiermdp.read()
-username = "root"
+fichiermdp = open("/root/.pwd" , "r")
+pwd = /root/.pwd
+
 
 import socket
-import paramiko
+import os
 
 # Tableau qui stockera les ip connectées """
 ipmachines = []
-
 
 for ping in range(1,254):
     adresse = "192.168.122." + str(ping)
@@ -34,18 +35,6 @@ for ping in range(1,254):
 		
 # Connexion SSH vers les machines connectées
 for ip in ipmachines:
-    print(ip)
-    print(pwd)
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(ip, 22, username, pwd)
-    stdin, stdout, stderr = ssh.exec_command(ls)
-
-    lines = stdout.readlines()
-
-    print(lines)
-    client.close()
-
-
-
-	
+    print("Le PC " + ip + " est connecté \n")
+    # Dépot de la clé publique sur le client
+    cmd= ("sshpass -f /root/.pwd ssh-copy-id -i /root/.ssh/id_rsa.pub root@" + ip)
