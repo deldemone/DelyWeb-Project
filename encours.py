1a5f069229def98eb1b352c4eb1cbaf1bf84b8dc
Delphine Durand <delphine.demongeot@gmail.com>
2021-01-20 19:23:46 +0100

    Journalisation et numero de requête

--- encours.py
+++ encours.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 ###Description: Permet de scanner le reseau d'un domaine et de parametrer les clients à distance"""
 
@@ -9,15 +10,45 @@ __status__ = "En cours de rédaction"
 
 # Variables globales
 fichiermdp = open("/root/.pwd" , "r")
-pwd = /root/.pwd
-
+chaine_alea = ""
 
 import socket
 import os
+import logging
+import random
+
+from string import punctuation, ascii_letters, digits
+
+
+def aleatoireString(nbcar, type):
+    symbols = ascii_letters + digits
+    Str_aleatoire = random.SystemRandom()
+    password = "".join(Str_aleatoire.choice(symbols) for i in range(nbcar))
+    chaine_alea = type + password
+    print("STRING=" + chaine_alea)
+
+
+	
+	
+# Log : DEBUG
+
+logger = logging.getLogger('chaine_alea')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('debug.log')
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+# logger.debug('Information Debug')
+# logger.info('Information Info')
+# logger.warning('Avertissement')
+# logger.error('Message d’erreur')
+# logger.critical('Erreur grave')
 
 # Tableau qui stockera les ip connectées """
 ipmachines = []
-
+logger.info('Le scanner ip a démarré')
 for ping in range(1,254):
     adresse = "192.168.122." + str(ping)
     socket.setdefaulttimeout(1)
@@ -32,9 +63,14 @@ for ping in range(1,254):
 
     if (hostname != None):
         ipmachines.append(adresse)
+
 		
 # Connexion SSH vers les machines connectées
 for ip in ipmachines:
-    print("Le PC " + ip + " est connecté \n")
+    nbcar = 8  
+    type = "REQ"  
+    aleatoireString(nbcar, type)
+    logger.info(chaine_alea + "Le PC " + ip + " est connecté")
     # Dépot de la clé publique sur le client
     cmd= ("sshpass -f /root/.pwd ssh-copy-id -i /root/.ssh/id_rsa.pub root@" + ip)
+    os.system(cmd)
