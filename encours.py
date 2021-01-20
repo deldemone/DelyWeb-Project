18df5c37984482b81bc68153ab77c83cc1b5a04d
DelDemone <delphine.demongeot@gmail.com>
2021-01-20 08:08:54 +0100

    Premiere tentative co ssh KO

--- encours.py
+++ encours.py
@@ -5,8 +5,13 @@ __version__ = "1.0.0"
 __email__ = "delphine.demongeot@gmail.com"
 __status__ = "En cours de rédaction"
 
+# Variables globales
+fichiermdp = open('/root/.pwd')
+pwd = fichiermdp.read()
+username = "root"
 
 import socket
+import paramiko
 
 # Tableau qui stockera les ip connectées """
 ipmachines = []
@@ -26,6 +31,21 @@ for ping in range(1,254):
 
     if (hostname != None):
         ipmachines.append(adresse)
-
+		
+# Connexion SSH vers les machines connectées
 for ip in ipmachines:
     print(ip)
+    print(pwd)
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, 22, username, pwd)
+    stdin, stdout, stderr = ssh.exec_command(ls)
+
+    lines = stdout.readlines()
+
+    print(lines)
+    client.close()
+
+
+
+	
