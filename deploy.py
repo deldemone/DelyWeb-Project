0d3c521eca0aa66f91c608dfc9fbc7ae96fef590
Delphine Durand <delphine.demongeot@gmail.com>
2021-01-31 22:36:12 +0100

    amélioration de la sortie console & log

--- deploy.py
+++ deploy.py
@@ -31,7 +31,11 @@ args = parser.parse_args()
 
 userID = args.userID
 password = args.pwd
-
+OK = " ##  OK  ## *** DIST - "
+KO = " ##  KO  ## *** DIST - "
+SO = " ##  SO  ## *** DIST - "
+INFO = " ## INFO ## *** DIST - "
+STEP = " ## STEP ## *** DIST - "
 # Suppression de l'ancien stagiaire et de son home
 readPasswd = open("/etc/passwd", "r")	# ouverture du fichier utilisateur en lecture seule
 lignes = readPasswd.readlines()
@@ -41,26 +45,26 @@ def suppOldUser():
         if "STGS" in ligne:
             separateur = ":"
             userOld = separateur.join(ligne.split(separateur)[0:1])
-            MSG = "Un user à supprimer : " + str(userOld)
+            MSG = OK + "Un user à supprimer : " + str(userOld)
             logging.info(MSG)
             cmd = "userdel -f " + userOld
             try:
                 os.system(cmd)
-                MSG = " ### SUCCESS -Ancien utilisateur " + userOld + " a été supprimé."
+                MSG = OK + "Ancien utilisateur " + userOld + " a été supprimé."
                 print(MSG)
                 logging.info(MSG)
                 shutil.rmtree("/home/" + userOld)
-                MSG = " ### SUCCESS -home de l'ancien utilisateur " + userOld + " a été supprimé."
+                MSG = OK + "/home de l'ancien utilisateur " + userOld + " a été supprimé."
                 PresenceOldUser =  True
                 print(MSG)
                 logging.info(MSG)
                 break
             except Exception as e:
                 print(e)
-                print(" ### Un pb est survenu lors de la suppression ancien user")
+                print(KO + "Un pb est survenu lors de la suppression ancien user")
 
     if ( PresenceOldUser == False ):
-        MSG = " ### SO - Aucun ancien utilisateur à supprimer."
+        MSG = SO + "-Aucun ancien utilisateur à supprimer."
         print(MSG)
         logging.info(MSG)
 
@@ -70,13 +74,13 @@ def addNewUser(userID, password):
     try:
         cmd = 'useradd -m ' + userID + '; echo \"' + userID + '\":' + password + ' | chpasswd' 
         os.system(cmd)
-        MSG = " ### SUCCESS -Nouvel utilisateur " + userID + " a été créé."
+        MSG = OK + "Nouvel utilisateur " + userID + " a été créé.\n" + OK + "/Home " + userID + " a été créé.\n" + OK + "PWD sécurisé déployé pour " + userID + ".\n"
         print(MSG)
     except Exception as e:
         print(e)
-        print(" ### Un pb est survenu lors de la suppression ancien user")
+        print(KO + "Un pb est survenu lors de la suppression ancien user")
 		
-		
+print(STEP + "Exécution du script de déploiement")
+
 suppOldUser()
 addNewUser(userID, password)
-exit()
